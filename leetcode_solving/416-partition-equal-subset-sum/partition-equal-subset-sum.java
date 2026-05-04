a5f93e1725e0fccac52e4e7b8fe84d354e3c529b
Vivek Rai <181317278+Vivekrai9@users.noreply.github.com>
2026-05-05 00:17:34 +0530

    Time: 111 ms (9.52%) | Memory: 75.7 MB (18.33%) - LeetSync

---
+++ leetcode_solving/416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        }
+        int target=0;
+        if(sum%2 ==0 ){
+            target = sum/2;
+        }
+        else{
+            return false;
+        }
+
+
+        Boolean [][]dp = new Boolean[nums.length+1][target+1];
+
+        return solve(0,target, nums,dp);  
+    }
+
+    public boolean solve(int i,int target , int[] nums, Boolean [][]dp){
+
+        if(target==0) return true;
+        if(i == nums.length) return false;
+
+        if(dp[i][target] != null ) return dp[i][target];
+
+        boolean pick = false;
+        if(nums[i]<= target){
+            pick = solve(i+1, target - nums[i], nums,dp);
+        }
+
+        boolean notpick = solve(i+1,target, nums,dp);
+
+        return dp[i][target]= pick || notpick;
+
+    }
+}
