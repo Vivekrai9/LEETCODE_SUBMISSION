2773976f65874a5b8ee8121d210aa10782e4b570
Vivek Rai <181317278+Vivekrai9@users.noreply.github.com>
2026-05-06 12:30:10 +0530

    Time: 637 ms (71.94%) | Memory: 0B (100.00%) - LeetSync

---
+++ leetcode_solving/184-department-highest-salary/department-highest-salary.sql
@@ -0,0 +1,9 @@
+select Department, Employee, Salary from
+(
+    select d.name as Department, e.name as Employee, e.salary as Salary, dense_rank() over (partition by d.id order by e.salary desc) as r
+    from Employee e
+    left join
+    Department d 
+    on e.departmentId  = d.id 
+) temp
+where r = 1
