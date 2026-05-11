b668eb381a01d749da70a44de36e65d4a1719b6d
Vivek Rai <181317278+Vivekrai9@users.noreply.github.com>
2026-05-11 12:14:33 +0530

    Time: 2 ms (83.17%) | Memory: 43.9 MB (56.73%) - LeetSync

--- leetcode_solving/956-number-of-music-playlists/number-of-music-playlists.java
+++ leetcode_solving/956-number-of-music-playlists/number-of-music-playlists.java
@@ -1,48 +1,31 @@
 
 class Solution {
-
-    int n, goal, k;
     int MOD = 1_000_000_007;
 
-
     public int numMusicPlaylists(int n, int goal, int k) {
-        this.n = n;
-        this.goal = goal;
-        this.k = k;
-
         long[][] dp = new long[goal+1][n+1];
 
-        for(int i=0; i< goal+1; i++) {
-             
+        for(int i=0; i< goal+1; i++) { 
             Arrays.fill(dp[i], -1);
         }
 
-        return (int) recurse(goal, 0,dp );
+        return (int) solve(goal, 0,dp,n,k );
     }
 
+    long solve(int currGoal, int oldSongs, long[][] dp, int n, int k) {
 
-    long recurse(int currGoal, int oldSongs, long[][] dp) {
+        if(currGoal == 0 && oldSongs == n) return 1;
+        if(currGoal == 0 || oldSongs > n) return 0;
 
-        if(currGoal == 0 && oldSongs == n) {
-            return 1;
-        }
-        if(currGoal == 0 || oldSongs > n) {
-            return 0;
-        }
-
-        if(dp[currGoal][oldSongs]!=-1) {
+        if(dp[currGoal][oldSongs] != -1) {
             return dp[currGoal][oldSongs];
         }
 
-        long newSong = ( n - oldSongs) * recurse(currGoal-1, oldSongs+1, dp);
-        // newSong = newSong % MOD;
-
-
+        long newSong = ( n - oldSongs) * solve(currGoal-1, oldSongs+1, dp,n,k);
+       
         long Repeat = 0;
         if(oldSongs > k) {
-
-            Repeat = ((oldSongs - k) * recurse(currGoal-1, oldSongs ,dp)) % MOD;
-
+            Repeat = ((oldSongs - k) * solve(currGoal-1, oldSongs ,dp,n,k)) % MOD;
         }
 
         return dp[currGoal][oldSongs] = ( newSong + Repeat ) % MOD;
