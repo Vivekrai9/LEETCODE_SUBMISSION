41f882dbfb39a025e757ebf373823781afeb7eff
Vivek Rai <181317278+Vivekrai9@users.noreply.github.com>
2026-05-15 09:44:53 +0530

    Time: 393 ms (7.99%) | Memory: 237.3 MB (6.92%) - LeetSync

--- leetcode_solving/300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ leetcode_solving/300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,36 +1,30 @@
 class Solution {
-
-    public int f(int i, int prev , int[][]dp , int [] nums){
-        if(i >= nums.length){
-            return 0;
-        }
-        if(dp[i][prev+1] != -1){
-            return dp[i][prev+1];
-        }
-
-        int pick=0, not_pick=0;
-
-        if(prev == -1 || nums[i] > nums[prev]){
-            pick = 1 + f(i+1,i,dp,nums);
-        }
-
-        not_pick = f(i+1,prev,dp,nums);
-
-        return dp[i][prev+1]= Math.max(pick , not_pick);
-
-    }
     public int lengthOfLIS(int[] nums) {
-        int n= nums.length;
-        int[][]dp = new int[n][n+1];
 
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i], -1);
+        int [][]dp = new int[nums.length+1][nums.length+1];
+
+        for(int [] r: dp){
+            Arrays.fill(r,-1);
+
         }
 
-        return f(0, -1, dp , nums);
+        return solve(0,-1,nums,dp);
+    }
+
+    public int solve(int i, int prev , int [] nums,int [][]dp){
+        if(i>= nums.length) return 0;
+
+        if(dp[i][prev+1] != -1) return dp[i][prev+1];
 
 
-        
+        int pick=0;
+        if((prev == -1 ) || nums[i] > nums[prev]){
+            pick= 1+ solve(i+1,i, nums,dp);
+        } 
+
+        int notpick= solve(i+1,prev, nums,dp);
+
+
+        return dp[i][prev+1]= Math.max(pick ,notpick);
     }
 }
-
