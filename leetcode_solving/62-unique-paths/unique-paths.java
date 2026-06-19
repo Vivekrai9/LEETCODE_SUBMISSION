0ec33ea88d39cc13a899e0034f805dc46c50d0cc
Vivek Rai <181317278+Vivekrai9@users.noreply.github.com>
2026-06-19 17:57:03 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (81.48%) - LeetSync

---
+++ leetcode_solving/62-unique-paths/unique-paths.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int[] r : dp) {
+            Arrays.fill(r, -1);
+        }
+
+        return solve(m - 1, n - 1, dp);
+
+    }
+
+    public int solve(int i, int j, int[][] dp) {
+        if (i == 0 && j == 0) return 1;
+        if (i < 0 || j < 0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        return dp[i][j] = solve(i, j - 1, dp) + solve(i - 1, j, dp);
+    }
+
+}
