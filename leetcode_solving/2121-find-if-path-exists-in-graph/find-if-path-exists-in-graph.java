3596fe5f44885316efd71461b46a0d6cbd60dfe1
Vivek Rai <181317278+Vivekrai9@users.noreply.github.com>
2026-07-15 01:46:55 +0530

    Time: 41 ms (91.72%) | Memory: 282.2 MB (42.35%) - LeetSync

--- leetcode_solving/2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ leetcode_solving/2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,33 +1,30 @@
+
 class Solution {
-
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-
-        if (source == destination)
-            return true;
+        if (source == destination) return true;
 
         List<Integer>[] adj = new ArrayList[n];
-        for (int i = 0; i < n; i++)
-            adj[i] = new ArrayList<>();
+        for (int i = 0; i < n; i++) adj[i] = new ArrayList<>();
 
         for (int[] e : edges) {
-            int u = e[0], v = e[1];
-            adj[u].add(v);
-            adj[v].add(u);
+            adj[e[0]].add(e[1]);
+            adj[e[1]].add(e[0]);
         }
 
         boolean[] visited = new boolean[n];
-        return dfs(adj, source, destination, visited);
-    }
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(source);
+        visited[source] = true;
 
-    private boolean dfs(List<Integer>[] adj, int curr, int dest, boolean[] visited) {
-        if (curr == dest) return true;
-        if (visited[curr])
-            return false;
-        visited[curr] = true;
-        
-        for (int nei : adj[curr]) {
-            if (dfs(adj, nei, dest, visited))
-                return true;
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            if (cur == destination) return true;
+            for (int nei : adj[cur]) {
+                if (!visited[nei]) {
+                    visited[nei] = true;
+                    q.add(nei);
+                }
+            }
         }
         return false;
     }
