408b2e6193db32b518a4214cda4aa29b888c1ef9
Vivek Rai <181317278+Vivekrai9@users.noreply.github.com>
2026-08-31 20:56:31 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (55.59%) - LeetSync

---
+++ leetcode_solving/82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+
+        ListNode dummy = new ListNode();
+        dummy.next=head;
+
+        ListNode prev = dummy;
+        ListNode curr= head;
+
+
+        while(curr != null){
+
+            if(curr.next!= null && curr.val == curr.next.val){
+
+                int duplicatedValue = curr.val;
+
+                while( curr != null && curr.val == duplicatedValue ){
+                    curr=curr.next;
+                }
+
+                prev.next =curr;
+            }
+
+            else{
+                prev = curr;
+                curr = curr.next;
+            }
+        }
+
+        return dummy.next;
+        
+    }
+}
